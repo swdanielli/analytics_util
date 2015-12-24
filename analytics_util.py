4a741ec799c1efcdc43b328e5cab9d7302605456
swdanielli <swdanielli@github.com>
2015-12-24 18:49:24 -0500

    commit

--- analytics_util.py
+++ analytics_util.py
@@ -1,5 +1,23 @@
 #! /usr/bin/env python
 
+import datetime
+import gzip
+import re
+
+def chunks(arr, chunk_size):
+  for i in xrange(0, len(arr), chunk_size):
+    yield arr[i:i+chunk_size]
+
+def date_range_gen(start_date, end_date):
+  for n in range(int((end_date - start_date).days)):
+    yield start_date + datetime.timedelta(n)
+
+def filter_event(event, keywords):
+  for keyword in keywords:
+    if keyword in event:
+      return True
+  return False
+
 def load_csv_like(filename, delimiter):
   header = None
   for line in open(filename):
@@ -7,3 +25,7 @@ def load_csv_like(filename, delimiter):
       yield (header, re.split(delimiter, line.strip()))
     else:
       header = re.split(delimiter, line.strip())
+
+def load_users(user_list):
+  with open(user_list) as f:
+    return [int(re.split('\t', line.strip())[0]) for line in f.readlines()]
