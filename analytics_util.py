c4fa6a0f567378dba7f5866ce79ec2d1b5f422b6
Shang-Wen Li <swli@teddy.csail.mit.edu>
2015-12-25 20:33:30 -0500

    commit

--- analytics_util.py
+++ analytics_util.py
@@ -1,7 +1,9 @@
 #! /usr/bin/env python
 
 import datetime
+from dateutil.parser import parse
 import gzip
+import json
 import re
 
 def chunks(arr, chunk_size):
@@ -18,6 +20,17 @@ def filter_event(event, keywords):
       return True
   return False
 
+def load_time_sorted_event(event_filename, keywords, filtered_function=lambda x: False):
+  events = []
+  for line in open(event_filename):
+    if not filter_event(line, keywords):
+      continue
+    event = json.loads(line)
+    if filtered_function(event):
+      continue
+    events.append(event)
+  return sorted(events, key=lambda x:parse(x['time']))
+
 def load_csv_like(filename, delimiter):
   header = None
   for line in open(filename):
@@ -29,3 +42,6 @@ def load_csv_like(filename, delimiter):
 def load_users(user_list):
   with open(user_list) as f:
     return [int(re.split('\t', line.strip())[0]) for line in f.readlines()]
+
+def time_diff(start_time, end_time):
+  return (parse(end_time)-parse(start_time)).total_seconds()
